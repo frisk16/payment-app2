66ee3de5add2ab4bf1d7083e4f6d497d1ac59326
gear <test-n18@hotmail.co.jp>
2024-08-10 18:58:35 +0900

    SSL有効化設定等

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        // SSL通信有効化
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
