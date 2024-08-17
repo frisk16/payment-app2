dc9dd486c7f43250b523b899e59fbcb90565b115
gear <test-n18@hotmail.co.jp>
2024-08-17 17:51:33 +0900

    PaymentController 修正

--- resources/js/Components/Form/PaymentsSearchForm.tsx
+++ resources/js/Components/Form/PaymentsSearchForm.tsx
@@ -51,7 +51,7 @@ const PaymentsSearchForm: FC<PaymentsPageProps> = memo((props) => {
                         <PrimaryButton
                             as="a"
                             w={{ base: "100%", md: "120px" }}
-                            href={`?year=${year}&month=${month}&keyword=${paymentData!.keyword}&maxPrice=${paymentData!.maxPrice}&minPrice=${paymentData!.minPrice}&order=${order}&page=1`}
+                            href={`?year=${year}&month=${month}&keyword=${paymentData!.keyword}&maxPrice=${paymentData!.maxPrice}&minPrice=${paymentData!.minPrice}&order=${order}`}
                         >
                             検索
                         </PrimaryButton>
--- resources/js/Layouts/PaymentsPageLayout.tsx
+++ resources/js/Layouts/PaymentsPageLayout.tsx
@@ -4,7 +4,7 @@ import PaymentsPageMain from "@/Components/Main/PaymentsPageMain";
 import { PaymentsPageProps } from "@/types/page/PaymentsPage";
 import { FC } from "react";
 
-const PaymentsPageLayout: FC<PaymentsPageProps> = (props) => { 
+const PaymentsPageLayout: FC<PaymentsPageProps> = (props) => {
     return (
         <>
             <PaymentsPageHeader
